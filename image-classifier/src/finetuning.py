d10d645f6810290665cebc698befc07a04d72571
Irshad Mohammed <creativeishu@gmail.com>
2017-03-20 13:43:24 -0500

    first attempt on fine tuning with keras 2.0

--- image-classifier/src/finetuning.py
+++ image-classifier/src/finetuning.py
@@ -4,27 +4,26 @@ import os
 import h5py
 from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
-from keras.models import Sequential
-from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.models import Sequential, Model
+from keras.layers import ZeroPadding2D
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
-
-# Forcing it to use theono as the backend
 from keras import backend as K
-K.set_image_dim_ordering('th')
+from keras.applications.vgg16 import VGG16
 
 np.random.seed(1234)
-
+from sys import argv, exit
 #==============================================================================
 
-datapath = '/Users/%s/Dropbox/irshad2janu/deeplearning_datasets/image_classifiers/'%os.getlogin()
-folder = datapath+'catsdogs/'
+folder = argv[1]
 
 train_data_dir = folder+'train'
 validation_data_dir = folder+'validation'
-weights_path = datapath+'vggfiles/vgg16_weights.h5'
+weights_path = folder+'../vggfiles/vgg16_weights.h5'
 
 nb_class = 2
-img_width, img_height = 150, 150
+img_width, img_height = 224, 224
 nb_train_samples = 2222
 nb_validation_samples = 1222
 
@@ -32,48 +31,55 @@ nb_validation_samples = 1222
 Train_bottleneck = False
 nb_filters = 32
 batch_size = 32
-n_epoch = 5
+n_epoch = 50
+
+if (K.image_data_format()=='channels_last'):
+    inputshape = (img_width, img_height, 3)
+elif (K.image_data_format()=='channels_first'):
+    inputshape = (3, img_width, img_height)
+else:
+    print "Invalid  data format: ", K.image_data_format()
+    exit()
 
 #==============================================================================
 
 def load_vgg16model(weights_path):
 
     model = Sequential()
-    model.add(ZeroPadding2D((1, 1), input_shape=(3, img_width, img_height)))
-
-    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
+    model.add(ZeroPadding2D((1,1),input_shape=inputshape))
+    model.add(Conv2D(64, (3, 3), activation='relu', name='conv1_1'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
+    model.add(Conv2D(64, (3, 3), activation='relu', name='conv1_2'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
+    model.add(Conv2D(128, (3, 3), activation='relu', name='conv2_1'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
+    model.add(Conv2D(128, (3, 3), activation='relu', name='conv2_2'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1'))
+    model.add(Conv2D(256, (3, 3), activation='relu', name='conv3_1'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
+    model.add(Conv2D(256, (3, 3), activation='relu', name='conv3_2'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
+    model.add(Conv2D(256, (3, 3), activation='relu', name='conv3_3'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1'))
+    model.add(Conv2D(512, (3, 3), activation='relu', name='conv4_1'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2'))
+    model.add(Conv2D(512, (3, 3), activation='relu', name='conv4_2'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3'))
+    model.add(Conv2D(512, (3, 3), activation='relu', name='conv4_3'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1'))
+    model.add(Conv2D(512, (3, 3), activation='relu', name='conv5_1'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2'))
+    model.add(Conv2D(512, (3, 3), activation='relu', name='conv5_2'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
+    model.add(Conv2D(512, (3, 3), activation='relu', name='conv5_3'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     assert os.path.exists(weights_path), 'Model weights not found (see "weights_path" variable in script).'
@@ -90,11 +96,14 @@ def load_vgg16model(weights_path):
 
 #------------------------------------------------------------------------------
 
-model = load_vgg16model(weights_path)
+# model = load_vgg16model(weights_path)
+
+base_model = VGG16(weights='imagenet', include_top=False, input_shape=inputshape)
+print base_model.summary()
 print
 print "VGG model loaded!!!"
 print
-
+# exit()
 #==============================================================================
 
 if Train_bottleneck:
@@ -173,7 +182,7 @@ top_model.add(Dense(1, activation='sigmoid'))
 top_model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 print top_model.summary()
 top_model.fit(train_data, train_labels,
-          nb_epoch=n_epoch, batch_size=batch_size,
+          epochs=5, batch_size=batch_size,
           validation_data=(validation_data, validation_labels))
 print
 
@@ -181,7 +190,7 @@ print
 
 print "Joining VGG network to previously training fully connected layer"
 print
-model.add(top_model)
+model = Model(inputs=base_model.input, outputs=top_model(base_model.output))
 for layer in model.layers[:25]:
     layer.trainable = False
     
@@ -204,13 +213,13 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 train_generator = train_datagen.flow_from_directory(
         train_data_dir,
         target_size=(img_height, img_width),
-        batch_size=100,
+        batch_size=32,
         class_mode='binary')
 
 validation_generator = test_datagen.flow_from_directory(
         validation_data_dir,
         target_size=(img_height, img_width),
-        batch_size=100,
+        batch_size=32,
         class_mode='binary')
 
 #------------------------------------------------------------------------------
@@ -218,11 +227,7 @@ validation_generator = test_datagen.flow_from_directory(
 print
 print "Finally fine tuning the last block of VGG and the fully connected layer..."
 print
-model.fit_generator(
-        train_generator,
-        samples_per_epoch=nb_train_samples,
-        nb_epoch=n_epoch,
-        validation_data=validation_generator,
-        nb_val_samples=nb_validation_samples)
+model.fit_generator(train_generator,validation_data=validation_generator,\
+        steps_per_epoch=100, epochs=n_epoch, validation_steps=2)
 
 #==============================================================================
--- image-classifier/src/image_category_predict.py
+++ image-classifier/src/image_category_predict.py
@@ -1,221 +1,68 @@
-"""
-Models for object recognition. Based on VGG16 and VGG19.
-"""
-
+import numpy as np 
 import os
-import h5py
 import cv2
-import sys
-import numpy as np
-import json
+from sys import exit, argv
 
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg19 import VGG19
 
-from keras.preprocessing.image import ImageDataGenerator
-from keras import optimizers
-from keras.models import Sequential
-from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
-from keras.layers import Activation, Dropout, Flatten, Dense
-from keras.optimizers import SGD
+# from keras.backend import backend
+# from keras.backend import image_dim_ordering as K
+# print backend()
+# print K()
+# exit()
 
-# from keras import backend as K
-# K.set_image_dim_ordering('th')
+#==============================================================================
 
-
-
-__author__ = 'jverma'
-
-
-
-def VGG16(weights_path, inputshape):
+class Imagepredict(object):
 	"""
-	Loads and builds the VGG16 model for object recognition.
-
-	Parameters
-	----------
-	weights_path: path to the HD5 file containing the weights of the pre-trained model.
-	img_width: Width of the image.
-	img_height: Height of the image.
-
-	Returns
-	-------
-	pre-trained model.
+	Some comments
 	"""
-	model = Sequential()
-	model.add(ZeroPadding2D((1, 1), input_shape=inputshape))
-	model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
-	model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
-	model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+	def __init__(self, model='vgg19'):
 
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1'))
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
-	model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+		self.dict = self.get_dict()
 
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1'))
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2'))
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3'))
-	model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1'))
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2'))
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
-	model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-	model.add(Flatten())
-	model.add(Dense(4096, activation='relu'))
-	model.add(Dropout(0.5))
-	model.add(Dense(4096, activation='relu'))
-	model.add(Dropout(0.5))
-	model.add(Dense(1000, activation='softmax'))
-
-	model.load_weights(weights_path)
-	return model
+		if model=='vgg19':
+			self.model = VGG19(weights='imagenet', include_top=True)
+		elif model=='vgg16':
+			self.model = VGG16(weights='imagenet', include_top=True)
+		else:
+			print "Only two models are available: VGG16 or VGG19"
+			exit()
+		print "Class is initialised!!!"
 
 
+	def get_dict(self):
+		path = '/Users/%s/Dropbox/irshad2janu/deeplearning_datasets/image_classifiers/vggfiles/'%os.getlogin()
+		class_dict = {}
+		index_file = open(path+'synset_words.txt')
+		for i,line in enumerate(index_file):
+			record = line.rstrip().split(' ')
+			class_dict[i] = record[1:]
+		return class_dict
+
+	def get_image_input(self, imagepath, shape=(224, 224)):
+		im = cv2.resize(cv2.imread(imagepath), shape).astype(np.float32)
+		im = np.expand_dims(im, axis=0)
+		return im
 
 
-def VGG19(weights_path, inputshape):
-	"""
-	Loads and builds the VGG19 model for object recognition.
+	def predict_image(self, imagepath, k=5):
+		im = self.get_image_input(imagepath)
+		out = self.model.predict(im)
+		idxs = np.argsort(out[0])[::-1][:k]
+		for x in idxs:
+			print(self.dict[x]), out[0][x]
 
-	Parameters
-	----------
-	weights_path: path to the HD5 file containing the weights of the pre-trained model.
-	img_width: Width of the image.
-	img_height: Height of the image.
-
-	Returns
-	-------
-	pre-trained model.
-	"""
-	model = Sequential()
-	model.add(ZeroPadding2D((1,1),input_shape=inputshape))
-	model.add(Convolution2D(64, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(64, 3, 3, activation='relu'))
-	model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(128, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(128, 3, 3, activation='relu'))
-	model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(256, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(256, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(256, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(256, 3, 3, activation='relu'))
-	model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu'))
-	model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu'))
-	model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-	model.add(Flatten())
-	model.add(Dense(4096, activation='relu'))
-	model.add(Dropout(0.5))
-	model.add(Dense(4096, activation='relu'))
-	model.add(Dropout(0.5))
-	model.add(Dense(1000, activation='softmax'))
-
-	model.load_weights(weights_path)
-	return model
+#==============================================================================	
 
 
+if len(argv)==2:
+	input_image = argv[1]
+	ob = Imagepredict()
+	ob.predict_image(input_image)
+else:
+	print "Usage: python <script.py> <image_file_path>"
 
-
-
-def category_predict(input_image, model_type='vgg16', img_width=224, img_height=224, k=5):
-	"""
-	Prints top categories for the input image.
-
-	Parameters
-	----------
-	input_image: The image to be recognized.
-	model_type: Type of model to be used, either vgg16 or vgg19.
-	img_width: Width of the image.
-	img_height: Height of the image.
-	k: Number of categories to be returned.
-	"""
-	path = '/Users/%s/Dropbox/irshad2janu/deeplearning_datasets/image_classifiers/vggfiles/'%os.getlogin()
-
-	class_dict = {}
-	index_file = open(path+'synset_words.txt')
-	for i,line in enumerate(index_file):
-		record = line.rstrip().split(' ')
-		class_dict[i] = record[1:]
-
-	im = cv2.resize(cv2.imread(input_image), (224, 224)).astype(np.float32)
-	# im[:,:,0] -= 103.939
-	# im[:,:,1] -= 116.779
-	# im[:,:,2] -= 123.68
-	im = im.transpose((2,0,1))
-	im = np.expand_dims(im, axis=0)
-	inputshape = tuple(im.shape[1:])
-	print "Input Shape: ", inputshape
-
-	if (model_type == 'vgg16'):
-		weights_path = path+'vgg16_weights.h5'
-		model = VGG16(weights_path, inputshape)
-	elif (model_type == 'vgg19'):
-		weights_path = path+'vgg19_weights.h5'
-		model = VGG19(weights_path, inputshape)
-	else:
-		print "Choose model_type vgg16 or vgg19."
-		return None
-
-	sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-	model.compile(optimizer=sgd, loss='categorical_crossentropy')
-	out = model.predict_proba(im)
-
-	idxs = np.argsort(out[0])[::-1][:k]
-	for x in idxs:
-		print(class_dict[x]), out[0][x]
-	return None
-
-
-
-
-
-input_image = sys.argv[1]
-category_predict(input_image, model_type='vgg19')
-
-
-
-
+#==============================================================================
