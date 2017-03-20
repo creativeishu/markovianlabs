0fd52d09f80e02a2094f808fa33594d86858a587
Irshad Mohammed <creativeishu@gmail.com>
2017-03-20 11:40:58 -0500

    sample generator CNN debugging: 1

--- deeplearning-standards/sample_cnn_generator.py
+++ deeplearning-standards/sample_cnn_generator.py
@@ -1,3 +1,5 @@
+# A sample CNN network
+
 import os
 import h5py
 import numpy as np
@@ -5,81 +7,79 @@ import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Activation, Dropout, Flatten, Dense
-from keras import optimizers
 from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
+from keras import optimizers
 from sys import argv, exit
+from keras import backend as K
 
+__author__ = 'irshad'
 
-# dimensions of our images.
-img_width, img_height = 150, 150
+#==============================================================================
 
 folder = argv[1]
 train_data_dir = folder+'train'
 validation_data_dir = folder+'validation'
+savefilename = folder+'savedmodels/firstattempt.hdf5'
 
-nb_train_samples = 2222
-nb_validation_samples = 1222
-nb_epoch = 50 # originally 50
+img_width, img_height = 150, 150
+nb_epoch = 50
 
-#Train_Bottleneck = False
-nb_class = 2
+# nb_class = 2
+if (K.image_data_format()=='channels_last'):
+	inputshape = (img_width, img_height, 3)
+elif (K.image_data_format()=='channels_first'):
+	inputshape = (3, img_width, img_height)
+else:
+	print "Invalid  data format: ", K.image_data_format()
+	exit()
+
+#==============================================================================
+
+print "============================================"
+print "Data Format: ", K.image_data_format()
+print "Input shape: ", inputshape
+# print "Number of classes: ", nb_class
+print "============================================"
+
+#==============================================================================
 
 simplemodel = Sequential()
-simplemodel.add(Conv2D(32, (3, 3), input_shape=(img_width, img_height, 3)))
-simplemodel.add(Activation('relu'))
+simplemodel.add(Conv2D(32, (3, 3), input_shape=inputshape, activation='relu'))
 simplemodel.add(MaxPooling2D(pool_size=(2, 2)))
 
-simplemodel.add(Conv2D(32, (3, 3)))
-simplemodel.add(Activation('relu'))
+simplemodel.add(Conv2D(32, (3, 3), activation='relu'))
 simplemodel.add(MaxPooling2D(pool_size=(2, 2)))
 
-simplemodel.add(Conv2D(64, (3, 3)))
-simplemodel.add(Activation('relu'))
+simplemodel.add(Conv2D(64, (3, 3), activation='relu'))
 simplemodel.add(MaxPooling2D(pool_size=(2, 2)))
 
 simplemodel.add(Flatten())
-simplemodel.add(Dense(64))
-simplemodel.add(Activation('relu'))
+simplemodel.add(Dense(64, activation='relu'))
 simplemodel.add(Dropout(0.5))
-simplemodel.add(Dense(1))
-simplemodel.add(Activation('sigmoid'))
+simplemodel.add(Dense(1, activation='sigmoid'))
 
-simplemodel.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
+simplemodel.compile(loss='binary_crossentropy', \
+	optimizer='adadelta', metrics=['accuracy'])
 
 print "Model loaded and compiled"
+print simplemodel.summary()
+
+#==============================================================================
 
 # this is the augmentation configuration we will use for training
-train_datagen = ImageDataGenerator(
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
+train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, \
+	zoom_range=0.2, horizontal_flip=True)
+test_datagen = ImageDataGenerator(rescale=1./255)
 
-# this is the augmentation configuration we will use for testing:
-# only rescaling
-test_datagen = ImageDataGenerator()
+train_generator = train_datagen.flow_from_directory(train_data_dir, \
+	target_size=(img_width, img_height), batch_size=32, class_mode='binary')
 
-train_generator = train_datagen.flow_from_directory(
-        train_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=32,
-        class_mode='binary')
+validation_generator = test_datagen.flow_from_directory(validation_data_dir, \
+	target_size=(img_width, img_height), batch_size=32, class_mode='binary')
 
-validation_generator = test_datagen.flow_from_directory(
-        validation_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=32,
-        class_mode='binary')
+simplemodel.fit_generator(train_generator, validation_data=validation_generator, \
+	steps_per_epoch=10, epochs=nb_epoch, validation_steps=2)
+# simplemodel.save(savefilename, overwrite=True)
 
-print simplemodel.summary()
-# simplemodel.fit_generator(
-#         train_generator,
-#         samples_per_epoch=nb_train_samples,
-#         nb_epoch=nb_epoch,
-#         validation_data=validation_generator,
-#         nb_val_samples=nb_validation_samples)
-
-simplemodel.fit_generator(generator=train_generator, validation_data=validation_generator, \
-    steps_per_epoch=35, epochs=50, validation_steps=10)
+#==============================================================================
