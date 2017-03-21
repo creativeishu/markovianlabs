89cc6c11e9aeb6626b700d96b0493bec418d04f7
Irshad Mohammed <creativeishu@gmail.com>
2017-03-21 00:05:27 -0500

    regression attempts

--- deeplearning-standards/regression_dense.py
+++ deeplearning-standards/regression_dense.py
@@ -6,12 +6,7 @@ import h5py
 from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
 from keras.models import Sequential
-from keras.layers import Convolution1D, Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.layers import Activation, Dropout, Flatten, Dense
-
-# Forcing it to use theono as the backend
-#from keras import backend as K
-#K.set_image_dim_ordering('th')
 
 np.random.seed(1234)
 
@@ -20,17 +15,16 @@ from sys import exit, argv
 #==============================================================================
 
 folder=argv[1]
-nlayers = 6
+nlayers = 5
 nf = 128
 act = 'relu'
 nepoch = 100
-batchsize = 32
+batchsize = 64
 loss = 'mean_squared_error'
 optimizer = 'adadelta'
-metrics = ['accuracy']
 inputfilename = folder+"/CMASS_50features_irshad.csv"
-savefilename = folder+'/model_%i_%i_%s.h5'%(nlayers, nf, act)
-
+savefilename = folder+'/model_%i_%i_%s.hdf5'%(nlayers, nf, act)
+val_split = 0.5
 
 #==============================================================================
 
@@ -39,16 +33,11 @@ dataframe = pandas.read_csv(filename)
 dataset = dataframe.values
 
 nCol = 50
-nn = int(len(dataset)*2/3)
-X_train = dataset[:nn,1:nCol+1]
-Y_train = dataset[:nn,nCol+1]
-X_test = dataset[nn:,1:nCol+1]
-Y_test = dataset[nn:,nCol+1]
+X_train = dataset[:,1:nCol+1]
+Y_train = dataset[:,nCol+1]
 
 print "Shape of X-train: ", X_train.shape
 print "Shape of Y-train: ", Y_train.shape
-print "Shape of X-test: ", X_test.shape
-print "Shape of Y-test: ", Y_test.shape
 
 #==============================================================================
 
@@ -57,10 +46,10 @@ def mymmodel(nlayers, nfilters, activations, dim):
 		print "Wrong input"
 		exit()
 	model = Sequential()
-	model.add(Dense(nCol, input_dim=dim, init='normal', activation=activations[0]))
-	for i in range(nlayers):
+	model.add(Dense(nfilters[0], activation=activations[0], kernel_initializer="normal", input_dim=dim))
+	for i in range(1, nlayers):
 		model.add(Dense(nfilters[i], activation=activations[i]))
-	model.add(Dense(1, init='normal'))
+	model.add(Dense(1, kernel_initializer="normal"))
 	return model
 
 nfilters = [nf] * nlayers
@@ -69,9 +58,10 @@ model = mymmodel(nlayers, nfilters, activation, X_train.shape[1])
 print model.summary()
 
 #==============================================================================
-#mean_absolute_percentage_error, mean_squared_error
-model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
-model.fit(X_train, Y_train, batch_size=batchsize, nb_epoch=nepoch, verbose=1, \
-	validation_data=(X_test, Y_test))
+
+model.compile(loss=loss, optimizer=optimizer)
+model.fit(X_train, Y_train, batch_size=batchsize, epochs=nepoch, verbose=1, \
+	validation_split=val_split)
 model.save(savefilename, overwrite=True)
+
 #==============================================================================
