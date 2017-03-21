fd49eb6e82e801c5b05b7064fc9a2787b8b9738c
Irshad Mohammed <creativeishu@gmail.com>
2017-03-20 19:51:28 -0500

    fine tuning file updated

--- image-classifier/src/finetuning.py
+++ image-classifier/src/finetuning.py
@@ -1,10 +1,11 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 import h5py
 from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
-from keras.models import Sequential, Model
+from keras.models import Sequential, Model, load_model
 from keras.layers import ZeroPadding2D
 from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
@@ -21,7 +22,6 @@ folder = argv[1]
 
 train_data_dir = folder+'train'
 validation_data_dir = folder+'validation'
-weights_path = folder+'../vggfiles/vgg16_weights.h5'
 
 nb_class = 2
 img_width, img_height = 150, 150
@@ -29,10 +29,13 @@ nb_train_samples = 2222
 nb_validation_samples = 1222
 
 # bottleneck training parameters
-Train_bottleneck = False
-nb_filters = 32
-batch_size = 32
+Train_bottleneck = True
+Train_topmodel = True
+
+batch_size = 16
 n_epoch = 50
+
+#==============================================================================
 
 if (K.image_data_format()=='channels_last'):
     inputshape = (img_width, img_height, 3)
@@ -44,97 +47,87 @@ else:
 
 #==============================================================================
 
-base_model = VGG16(weights='imagenet', include_top=False, input_shape=inputshape)
-print base_model.summary()
-print
-print "VGG model loaded!!!"
-print
+def save_bottlebeck_features():
+    datagen = ImageDataGenerator(rescale=1. / 255)
 
-# for i in range(len(base_model.layers)):
-    # print base_model.layers[i], i
-# exit()
+    # build the VGG16 network
+    model = VGG16(include_top=False, weights='imagenet')
+
+    generator = datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_train = model.predict_generator(
+        generator, nb_train_samples // batch_size)
+    np.save(open(folder+'savedmodels/bottleneck_features_train.npy', 'w'),
+            bottleneck_features_train)
+
+    generator = datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_validation = model.predict_generator(
+        generator, nb_validation_samples // batch_size)
+    np.save(open(folder+'savedmodels/bottleneck_features_validation.npy', 'w'),
+            bottleneck_features_validation)
+
+#------------------------------------------------------------------------------
+
+def train_top_model():
+    train_data = np.load(open(folder+'savedmodels/bottleneck_features_train.npy'))
+    nb_train_samples = len(train_data)
+    train_labels = []
+    for i in range(nb_class):
+        train_labels +=  list([i] * (nb_train_samples / nb_class))
+
+    validation_data = np.load(open(folder+'savedmodels/bottleneck_features_validation.npy'))
+    nb_validation_samples = len(validation_data)
+    validation_labels = []
+    for i in range(nb_class):
+        validation_labels +=  list([i] * (nb_validation_samples / nb_class))
+    
+    train_labels = np.array(train_labels)
+    validation_labels = np.array(validation_labels)
+    
+    print "Training data: ", train_data.shape
+    print "Validation data: ", validation_data.shape
+    model = Sequential()
+    model.add(Flatten(input_shape=train_data.shape[1:]))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
+
+    model.compile(optimizer='rmsprop',
+                  loss='binary_crossentropy', metrics=['accuracy'])
+    print model.summary()
+    model.fit(train_data, train_labels,
+              epochs=n_epoch,
+              batch_size=batch_size,
+              validation_data=(validation_data, validation_labels))
+    model.save(folder+'savedmodels/top_model.hdf5')
+    return model
 
 #==============================================================================
 
 if Train_bottleneck:
-    print "Training Bottleneck features..."
-    print
-    datagen = ImageDataGenerator(rescale=1./255)
-    generator = datagen.flow_from_directory(
-            train_data_dir,
-            target_size=(img_width, img_height),
-            batch_size=16,
-            class_mode=None,
-            shuffle=False)
-    bottleneck_features_train = base_model.predict_generator(generator, nb_train_samples)
-    np.save(open(folder+'savedmodels/bottleneck_features_train.npy', 'w'), \
-    	bottleneck_features_train)
+    save_bottlebeck_features()
 
-    generator = datagen.flow_from_directory(
-            validation_data_dir,
-            target_size=(img_width, img_height),
-            batch_size=16,
-            class_mode=None,
-            shuffle=False)
-    bottleneck_features_validation = base_model.predict_generator(generator, nb_validation_samples)
-    np.save(open(folder+'savedmodels/bottleneck_features_validation.npy', 'w'), \
-    	bottleneck_features_validation)
-
-#------------------------------------------------------------------------------
-
-print "Loading bottleneck features..."
-print
-train_data = np.load(open(folder+'savedmodels/bottleneck_features_train.npy'))
-nb_train_samples = len(train_data)
-train_labels = []
-for i in range(nb_class):
-    train_labels +=  list([i] * (nb_train_samples / nb_class))
-    
-validation_data = np.load(open(folder+'savedmodels/bottleneck_features_validation.npy'))
-nb_validation_samples = len(validation_data)
-validation_labels = []
-for i in range(nb_class):
-    validation_labels +=  list([i] * (nb_validation_samples / nb_class))
-
-train_labels = np.array(train_labels)
-validation_labels = np.array(validation_labels)
-
-print "Bottleneck features shape: Training set: ", train_data.shape, \
-		train_labels.shape
-print "Bottleneck features shape: Validation set: ",validation_data.shape, \
-		validation_labels.shape    
-print
-
-#------------------------------------------------------------------------------
-
-ind_train = np.arange(len(train_data))
-np.random.shuffle(ind_train)
-
-ind_validation = np.arange(len(validation_data))
-np.random.shuffle(ind_validation)
-
-train_data = train_data[ind_train]
-train_labels = train_labels[ind_train]
-
-validation_data = validation_data[ind_validation]
-validation_labels = validation_labels[ind_validation]
-
+if Train_topmodel:    
+    top_model = train_top_model()
+else:
+    top_model = load_model(folder+'savedmodels/top_model.hdf5')
+exit()
 #==============================================================================
 
-print "Training a fully connected layer with Bottleneck features..."
+base_model = VGG16(weights='imagenet', include_top=False, input_shape=inputshape)
+print base_model.summary()
 print
-top_model = Sequential()
-top_model.add(Flatten(input_shape=train_data.shape[1:]))
-top_model.add(Dense(nb_filters, activation='relu'))
-top_model.add(Dropout(0.5))
-top_model.add(Dense(1, activation='sigmoid'))
-
-top_model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
-print top_model.summary()
-top_model.fit(train_data, train_labels,
-          epochs=100, batch_size=batch_size,
-          validation_data=(validation_data, validation_labels))
-print
+print "VGG model loaded!!!"
+print    
 
 #==============================================================================
 
@@ -177,9 +170,9 @@ validation_generator = test_datagen.flow_from_directory(
 print
 print "Finally fine tuning the last block of VGG and the fully connected layer..."
 print
-# model.fit_generator(train_generator,validation_data=validation_generator,\
-        # steps_per_epoch=100, epochs=n_epoch, validation_steps=2)
+
 model.fit_generator(train_generator, steps_per_epoch=nb_train_samples // batch_size, \
     epochs=n_epoch, validation_data=validation_generator, validation_steps=nb_validation_samples // batch_size)
 
 #==============================================================================
+
