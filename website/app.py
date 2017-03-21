c5c0c70d5cadcc8c93089cd54c088b5ea7b183b2
Jverma <j.verma5@gmail.com>
2017-03-21 20:29:14 +0530

    website stub

---
+++ website/app.py
@@ -0,0 +1,72 @@
+import os
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template, jsonify
+
+from flask_util_js import FlaskUtilJs
+
+from werkzeug import secure_filename
+import json
+import subprocess
+
+from image_category_predict import Imagepredict
+
+
+
+UPLOAD_FOLDER = 'Uploads'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
+
+
+
+app = Flask(__name__)
+fujs = FlaskUtilJs(app)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.context_processor
+def inject_fujs():
+    return dict(fujs=fujs)
+    
+
+def allowed_file(filename):
+	return '.' in filename and filename.split('.',1)[1] in ALLOWED_EXTENSIONS
+
+
+def save_to_disk(file):
+	if (file and allowed_file(file.filename)):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		return filename
+	else:
+		return None
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+	if (request.method == 'POST'):
+		image_file = request.files['image_file']
+		imageFileName = save_to_disk(image_file)
+		
+		ob = Imagepredict()
+		res = ob.predict_image(os.path.join(app.config['UPLOAD_FOLDER'], imageFileName))
+		return jsonify(file_name=imageFileName, res=res)
+	else:
+		print "Request"
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
+
+
---
+++ website/image_category_predict.py
@@ -0,0 +1,74 @@
+import numpy as np 
+import os
+import cv2
+from sys import exit, argv
+
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg19 import VGG19
+
+from keras.optimizers import SGD
+
+#==============================================================================
+
+class Imagepredict(object):
+	"""
+	Some comments
+	"""
+
+	def __init__(self, model='vgg19'):
+
+		self.dict = self.get_dict()
+		sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+
+		if model=='vgg19':
+			self.model = VGG19(weights='imagenet', include_top=True)
+			self.model.compile(optimizer=sgd, loss='categorical_crossentropy')
+		elif model=='vgg16':
+			self.model = VGG16(weights='imagenet', include_top=True)
+			self.model.compile(optimizer=sgd, loss='categorical_crossentropy')
+		else:
+			print "Only two models are available: VGG16 or VGG19"
+			exit()
+		print "Class is initialised!!!"
+
+
+	def get_dict(self):
+		path = '/Users/%s/Dropbox/irshad2janu/deeplearning_datasets/image_classifiers/vggfiles/'%os.getlogin()
+		class_dict = {}
+		index_file = open(path+'synset_words.txt')
+		for i,line in enumerate(index_file):
+			record = line.rstrip().split(' ')
+			class_dict[i] = record[1:]
+		return class_dict
+
+
+	def get_image_input(self, imagepath, shape=(224, 224)):
+		im = cv2.resize(cv2.imread(imagepath), shape).astype(np.float32)
+		im[:,:,0] -= 103.939
+		im[:,:,1] -= 116.779
+		im[:,:,2] -= 123.68
+		im = np.expand_dims(im, axis=0)
+		return im
+
+
+	def predict_image(self, imagepath, k=5):
+		im = self.get_image_input(imagepath)
+		out = self.model.predict(im)
+		idxs = np.argsort(out[0])[::-1][:k]
+		results = [['class', 'prob']]
+		for x in idxs:
+			category = [self.dict[x], float(out[0][x])]
+			results.append(category)
+			# print(self.dict[x]), out[0][x]
+		return results
+
+#==============================================================================	
+
+if len(argv)==2:
+	input_image = argv[1]
+	ob = Imagepredict()
+	ob.predict_image(input_image)
+else:
+	print "Usage: python <script.py> <image_file_path>"
+
+#==============================================================================
