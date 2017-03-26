f14e7a2d48b793d246627c934d2e7948342e68c5
Jverma <j.verma5@gmail.com>
2017-03-26 19:54:14 +0530

    no new changes

--- website/static/javascript/image_reco.js
+++ website/static/javascript/image_reco.js
@@ -2,11 +2,14 @@ function extract_uploaded_file(data, textStatus, jqXHR) {
 	var file_name = data['file_name'];
 	// $('#result').text(file_name);
 	// console.log(data.res);
-	$('#result').append('<img id="dynamic" height="300" width="300">');
+	$('#result').empty();
+	$('#result').append('<div id="inputImage"></div>');
+	$('#inputImage').append('<img id="dynamic" height="500" width="500">');
 	$('#dynamic').attr('src', flask_util.url_for("uploaded_file", {filename: file_name}));
+	$('#result').append('<div id="resultTable"></div>');
 	for (var i=0; i<data.res.length; i++){
 		$('<p>',{
 		text: data.res[i]}
-		).appendTo('#result');
+		).appendTo('#resultTable');
 	}
 }
