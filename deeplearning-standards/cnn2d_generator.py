c1fedcddf82055a871031941d0bf7020904652fd
Irshad Mohammed <creativeishu@gmail.com>
2017-03-29 17:40:46 -0500

    simple convolutional network training file updated

--- deeplearning-standards/cnn2d_generator.py
+++ deeplearning-standards/cnn2d_generator.py
@@ -26,53 +26,24 @@ savefilename = folder+'savedmodels/cnn_small.hdf5'
 img_width, img_height = 224, 224
 nb_epoch = 50
 batch_size = 32
+nfilters = 64
+nlayers = 2
+nchannels = 3
+savemodel = True
 
-train_samples = 58787
-valid_samples = 28919
+loss = 'binary_crossentropy'
+optimizer = 'adadelta'
+metrics = ['accuracy']
 
-nb_class = 15
+#==============================================================================
 
 if (K.image_data_format()=='channels_last'):
-	inputshape = (img_width, img_height, 3)
+	inputshape = (img_width, img_height, nchannels)
 elif (K.image_data_format()=='channels_first'):
-	inputshape = (3, img_width, img_height)
+	inputshape = (nchannels, img_width, img_height)
 else:
 	print "Invalid  data format: ", K.image_data_format()
 	exit()
-
-#==============================================================================
-
-print "============================================"
-print "Data Format: ", K.image_data_format()
-print "Input shape: ", inputshape
-print "Number of classes: ", nb_class
-print "============================================"
-
-#==============================================================================
-
-model = Sequential()
-model.add(ZeroPadding2D((1,1),input_shape=inputshape))
-model.add(Conv2D(64, (3, 3), activation='relu', name='conv1_1'))
-model.add(ZeroPadding2D((1, 1)))
-model.add(Conv2D(64, (3, 3), activation='relu', name='conv1_2'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-model.add(ZeroPadding2D((1, 1)))
-model.add(Conv2D(128, (3, 3), activation='relu', name='conv2_1'))
-model.add(ZeroPadding2D((1, 1)))
-model.add(Conv2D(128, (3, 3), activation='relu', name='conv2_2'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-model.add(Flatten())
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(nb_class, activation='sigmoid'))
-
-model.compile(loss='binary_crossentropy', \
-	optimizer='adadelta', metrics=['accuracy'])
-
-print "Model loaded and compiled"
-print model.summary()
 
 #==============================================================================
 
@@ -82,14 +53,58 @@ train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, \
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(train_data_dir, \
-	target_size=(img_width, img_height), batch_size=batch_size, class_mode='categorical')
+	target_size=(img_width, img_height), batch_size=batch_size, \
+	class_mode='categorical')
 
 validation_generator = test_datagen.flow_from_directory(validation_data_dir, \
-	target_size=(img_width, img_height), batch_size=batch_size, class_mode='categorical')
+	target_size=(img_width, img_height), batch_size=batch_size, \
+	class_mode='categorical')
+
+train_samples = train_generator.samples 
+valid_samples = validation_generator.samples
+nb_class = max(train_generator.classes)+1
+
+#==============================================================================
+
+model = Sequential()
+model.add(ZeroPadding2D((1,1),input_shape=inputshape))
+for i in range(nlayers):
+	model.add(Conv2D(nfilters, (3, 3), activation='relu', name='conv2d_%i'%(i+1)))
+	model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(nfilters, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(nb_class, activation='sigmoid'))
+
+model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
+
+#==============================================================================
+
+print "============================================"
+print "Data Format: ", K.image_data_format()
+print "Input shape: ", inputshape
+print "Number of training samples: ", train_samples
+print "Number of validation samples: ", valid_samples
+print "Number of classes: ", nb_class
+print "Number of convolutional layers: ", nlayers
+print "Batch size: ", batch_size
+print "Number of fileters in convolutional layers: ", nfilters
+print "Loss: ", loss
+print "Optimizer: ", optimizer
+print "Metrics: ", metrics
+print "Number of epochs: ", nb_epoch
+print "Model will be saved at: ", savefilename
+print "============================================"
+print model.summary()
+
+#==============================================================================
 
 model.fit_generator(train_generator, validation_data=validation_generator, \
 	steps_per_epoch=train_samples//batch_size, epochs=nb_epoch, \
 	validation_steps=valid_samples/batch_size)
-model.save(savefilename, overwrite=True)
+
+if savemodel:
+	model.save(savefilename, overwrite=True)
 
 #==============================================================================
