7a2f352c7e659147e19fefd7f803cd50e442b857
Irshad Mohammed <creativeishu@gmail.com>
2017-04-05 12:07:36 -0500

    features file added

---
+++ image-classifier/source_codes/get_features.py
@@ -0,0 +1,33 @@
+from keras.applications.vgg19 import VGG19
+from keras.preprocessing import image
+from keras.applications.vgg19 import preprocess_input
+from keras.models import Model
+import numpy as np
+
+#==============================================================================
+
+def get_features(img_path, layername='fc1'):
+	base_model = VGG19(weights='imagenet')
+	print base_model.summary()
+	model = Model(inputs=base_model.input, \
+			outputs=base_model.get_layer(layername).output)
+
+	img = image.load_img(img_path, target_size=(224, 224))
+	x = image.img_to_array(img)
+	x = np.expand_dims(x, axis=0)
+	x = preprocess_input(x)
+	features = model.predict(x)
+	return features
+
+#==============================================================================
+
+if __name__ == " __main__":
+	from sys import argv, exit
+	
+	if len(argv)==2:
+		get_features(argv[1])
+	elif len(argv==3):
+		get_features(argv[1], argv[2])
+	else:
+		print "Usage: python get_features.py <ImagePath> <LayerName (OPTIONAL)>"
+		exit()
