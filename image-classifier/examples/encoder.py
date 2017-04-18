3112d189554833c912b2fff570f65ee22bb4440c
Irshad Mohammed <creativeishu@gmail.com>
2017-04-17 22:13:06 -0500

    encoder

--- image-classifier/examples/encoder.py
+++ image-classifier/examples/encoder.py
@@ -22,6 +22,8 @@ x_train = np.reshape(x_train, \
 x_test = np.reshape(x_test, \
 			(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1))
 
+x_train = x_train/255.0
+
 print "Training set: ", x_train.shape, y_train.shape
 print "Test set: ", x_test.shape, y_test.shape
 
